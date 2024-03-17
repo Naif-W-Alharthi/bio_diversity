17a7bbc17f997721d1dfbc31713b5168d12971a9
Amir Nozari <nozari.a2012@gmail.com>
2024-03-17 23:39:06 +0000

    More preprocessing and adding error margins

--- eda_amir.py
+++ eda_amir.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import os
+import scipy.stats as stats
+import numpy as np
 
 # # This code is done using a different methodology compared to my teammate Naif, where  he tries to have an automated
 # # code for all the files, I have instead gone through each file individually.
@@ -10,10 +12,12 @@ df1.drop(["Country Code", "Series Name", "Series Code"], axis=1, inplace=True)
 # "Series Code" repeat the same values over and over again. Plus by transforming these columns from Wide Format into
 # Long Format, they are also made redundant.
 
+df1 = df1.iloc[:-51]
+# The last 5 rows did not contain data, but instead where the data came from and other rows did not represent countries,
+# but instead represent groups of counties joined by different criteria.
+
 df1 = pd.melt(df1, id_vars=['Country Name'], var_name='Year', value_name='Agricultural land (% of land area)')
 
-df1 = df1.iloc[:-5]
-# The last 5 rows did not contain data, but instead where the data came from
 df1['Year'] = df1['Year'].str.split('[', expand=True)[0].str.strip()
 # Trimmed the year values so that they only display their year.
 df1 = df1.sort_values(by=['Country Name', 'Year'])
@@ -36,8 +40,9 @@ df3.drop(["Country Code", "Series Name", "Series Code"], axis=1, inplace=True)
 # "Series Code" repeat the same values over and over again. Plus by transforming these columns from Wide Format into
 # Long Format, they are also made redundant.
 
-df3 = df3.iloc[:-5]
-# The last 5 rows did not contain data, but instead where the data came from
+df1 = df1.iloc[:-51]
+# The last 5 rows did not contain data, but instead where the data came from and other rows did not represent countries,
+# but instead represent groups of counties joined by different criteria.
 
 df3 = pd.melt(df3, id_vars=['Country Name'], var_name='Year', value_name='Forest area (% of land area)')
 
@@ -50,10 +55,14 @@ print("")
 df4 = pd.read_csv('Biodiversity/' + "GHG emissions per kilogram produced.csv", header=0)
 df4.drop(["Year"], axis=1, inplace=True)
 # The column "Year" repeat the same value "2010" over and over again, and so is removed for being redundant.
+df4.rename(columns={"Entity": "Country Name"}, inplace=True)
+# renaming the columns in order to uniformize it with the rest of the files
 
 print("")
 
 df5 = pd.read_csv('Biodiversity/' + "global-living-planet-index.csv", header=0)
+df5.rename(columns={"Entity": "Country Name"}, inplace=True)
+# renaming the columns in order to uniformize it with the rest of the files
 
 print("")
 
@@ -63,8 +72,9 @@ df6.drop(["Country Code", "Series Name", "Series Code"], axis=1, inplace=True)
 # "Series Code" repeat the same values over and over again. Plus by transforming these columns from Wide Format into
 # Long Format, they are also made redundant.
 
-df6 = df6.iloc[:-5]
-# The last 5 rows did not contain data, but instead where the data came from
+df1 = df1.iloc[:-51]
+# The last 5 rows did not contain data, but instead where the data came from and other rows did not represent countries,
+# but instead represent groups of counties joined by different criteria.
 
 df6 = pd.melt(df6, id_vars=['Country Name'], var_name='Year', value_name='Tree Cover Loss (hectares)')
 
@@ -75,10 +85,16 @@ df6 = df6.sort_values(by=['Country Name', 'Year'])
 print("")
 
 df7 = pd.read_csv('Biodiversity/' + "wheat-yields.csv", header=0)
+df7 = df7.dropna(subset=["Code"])
+# Removing all none countries from the
 df7.drop(["Code"], axis=1, inplace=True)
 # These columns are dropped because "Code" is redundant when we have the countries name
 
-df_list = [df1,df2,df3,df4,df5,df6,df7]
+df7.rename(columns={"Entity": "Country Name", 'Wheat yield': 'Wheat Yield (tonnes/km2)'}, inplace=True)
+# renaming the columns in order to uniformize it with the rest of the files
+
+
+df_list = [df1, df2, df3, df4, df5, df6, df7]
 df_names = ['2_Clean Agricultural Land.csv',
             '2_Clean deforestation-co2-trade-by-product.csv',
             '2_Clean Forest Area.csv',
@@ -89,18 +105,11 @@ df_names = ['2_Clean Agricultural Land.csv',
 if not os.path.exists("Clean Biodiversity"):
     os.makedirs("Clean Biodiversity")
 
-df1.to_csv(os.path.join("Clean Biodiversity", '1_Long Agricultural Land.csv'), index=False)
-df2.to_csv(os.path.join("Clean Biodiversity", '1_Long deforestation-co2-trade-by-product.csv'), index=False)
-df3.to_csv(os.path.join("Clean Biodiversity", '1_Long Forest Area.csv'), index=False)
-df4.to_csv(os.path.join("Clean Biodiversity", '1_Long GHG emissions per kilogram produced.csv'), index=False)
-df5.to_csv(os.path.join("Clean Biodiversity", '1_Long global-living-planet-index.csv'), index=False)
-df6.to_csv(os.path.join("Clean Biodiversity", '1_Long Tree Cover Loss.csv'), index=False)
-df7.to_csv(os.path.join("Clean Biodiversity", '1_Long wheat-yields.csv'), index=False)
-
 # # Cleaning
-i=0
+i = 0
 for df in df_list:
-    df = df.apply(lambda x: x if x.name in ['Country Name', 'Entity', 'Products', 'Code'] else pd.to_numeric(x, errors='coerce'))
+    df = df.apply(
+        lambda x: x if x.name in ['Country Name', 'Entity', 'Products', 'Code'] else pd.to_numeric(x, errors='coerce'))
     for col in df.columns:
         if df[col].dtype in ['int64', 'float64']:
             df[col] = pd.to_numeric(df[col], errors='coerce').round(2)
@@ -109,8 +118,49 @@ for df in df_list:
     columns_to_keep = nan_percentage[nan_percentage <= 80].index
     df = df[columns_to_keep]
     df = df.dropna()
+    print(df.isnull().sum())
     df.to_csv(os.path.join("Clean Biodiversity", df_names[i]), index=False)
     i = i + 1
 
-# # 
+#
+df1 = pd.read_csv(os.path.join("Clean Biodiversity", '2_Clean Agricultural Land.csv'))
+df2 = pd.read_csv(os.path.join("Clean Biodiversity", '2_Clean deforestation-co2-trade-by-product.csv'))
+df3 = pd.read_csv(os.path.join("Clean Biodiversity", '2_Clean Forest Area.csv'))
+df4 = pd.read_csv(os.path.join("Clean Biodiversity", '2_Clean GHG emissions per kilogram produced.csv'))
+df5 = pd.read_csv(os.path.join("Clean Biodiversity", '2_Clean global-living-planet-index.csv'))
+df6 = pd.read_csv(os.path.join("Clean Biodiversity", '2_Clean Tree Cover Loss.csv'))
+df7 = pd.read_csv(os.path.join("Clean Biodiversity", '2_Clean wheat-yields.csv'))
+df_list = [df1, df2, df3, df4, df5, df6, df7]
 
+# # Statistics
+i = 0
+for df in df_list:
+    # print(df.describe())
+    # print(df.shape)
+    for col in df.columns:
+        if df[col].dtype in ['int64', 'float64'] and not col in ["Year", "Upper CI", "Lower CI"]:
+            # print(df[col].unique())
+            # print(df.value_counts(subset = df[col]))
+
+            # Sample size
+            n = len(df[col])
+
+            # Sample mean and standard deviation
+            sample_mean = np.mean(df[col])
+            sample_std = np.std(df[col], ddof=1)  # Use ddof=1 for sample standard deviation
+
+            # Confidence level (e.g., 95% confidence level)
+            confidence_level = 0.95
+
+            # Calculate the critical value (z-score for large sample sizes)
+            critical_value = stats.norm.ppf((1 + confidence_level) / 2)  # Two-tailed test
+
+            # Calculate the margin of error
+            margin_of_error = critical_value * (sample_std / np.sqrt(n))
+
+            margin_of_error_percentage = (margin_of_error / sample_mean) * 100
+
+            print(f"Margin of Error for Metric {df_names[i]}: ", margin_of_error_percentage.round(2))
+            i += 1
+
+print("")
